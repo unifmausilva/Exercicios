48d8d4c663bde2e7574dfb85d7beca6dcd85649e
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-05-19 11:55:29 +0000

    primeiro exercicio

---
+++ ex1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+ int numeros;
+  
+  printf("Digite a Quantidade de números do Array :");
+  scanf("%d", &numeros);
+
+int Array[numeros];
+
+printf("Digite os números do Array:", numeros);
+  for (int i = 0; i < numeros; i++){
+    printf("Número %d:", i+1);
+    scanf("%d", &Array[i]);
+  }
+
+  //soma
+  int soma = 0;
+  for (int i = 0; i < numeros; i++){
+    soma += Array[i];
+  }
+  printf("A soma dos números do Array é : %d\n", soma);
+}
