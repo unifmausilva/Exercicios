a4175af164cf3875de896657457550def6763137
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-05-22 14:45:07 +0000

    ultimo exercicio

---
+++ .ccls-cache/@home@runner@Exercicios/ex10.c
@@ -0,0 +1,20 @@
+/*
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+
+    printf("Hello World \n");
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Tempo de execução: %f segundos\n", cpu_time_used);
+
+    return 0;
+}
+*/
